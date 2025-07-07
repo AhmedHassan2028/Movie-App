4d00e59afdc47cba916268b0064357fd2eca2f4c
Ahmed Hassan <ahfa871@gmail.com>
2025-07-07 16:32:04 -0400

    July 7

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,53 @@
 import MovieCard from "../components/MovieCard"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { searchMovies } from "../services/api";
+import { getPopularMovies } from "../services/api";
+import "../css/Home.css"
+
 
 function Home() {
 
   const [searchQuery, setSearchQuery] = useState("");
+  const [movies,setMovies] = useState([])
+  const [error, setError] = useState(null)
+  const [loading, setLoading ] = useState(true)
 
-  const movies = [
-    {id: 1, title: "John Wick", release_date: "2020"},
-    {id: 2, title: "Terminator", release_date: "1999"},
-    {id: 3, title: "The Matrix", release_date: "1998"},
-  ]
+  useEffect (() => {
+    const loadPopularMovies = async () => {
+      try {
+        const PopularMovies = await getPopularMovies()   
 
-  const handleSearch = () => {
+        setMovies(PopularMovies)
+      } catch(err) {
+        console.log(err);
+        setError("Failed to load movies...")
+      }
+      finally {
+        setLoading(false)
+      }
+    }
+
+    loadPopularMovies();
+  }, [])
+
+  const handleSearch = async (e) => {
     e.preventDefault();
-    alert(searchQuery);
-    setSearchQuery("");
+    if (!searchQuery.trim()) return
+    if(loading) return
+    
+    setLoading(true)
+    try {
+      const searchResults = await searchMovies(searchQuery)
+      setMovies(searchResults)
+      setError(null)
+    } catch(err) {
+      console.log(err);
+      setError("Failed to search movies...")
+    } finally {
+      setLoading(false)
+    }
+
+    
   }
 
   return (
@@ -28,15 +61,19 @@ function Home() {
       <button type="submit" className="search-button">Search</button>
     </form>
 
+
+
+
+  {loading ? (<p>Loading...</p>) : error ? (<p>{error}</p>) :
+   (
     <div className="movies-grid">
-      {movies.map (
-        (movie) => 
-          movie.title.toLowerCase().startsWith(searchQuery) && (
-          <MovieCard movie={movie} key={movie.id}/>
-        )
-      )}
+      {movies.map ((movie) => (
+          <MovieCard movie={movie} key={movie.id}/> 
+      ))}
       
     </div>
+  )}
+    
   </div>
   )
 }
